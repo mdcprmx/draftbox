4272789e002057553b35b2b980cf5c357f2c183d
Tanja Anneliese <tanjaann@si-f2.yks.21-school.ru>
2024-01-27 18:49:07 +0900

    modified s21_cat_alt3.c

--- src/s21_cat_alt3.c
+++ src/s21_cat_alt3.c
@@ -8,38 +8,34 @@
 
 void scenario_file_doesnt_exist(int file_descr);
 void scenario_no_arguments();
-void arguments_parser(int argc, char **argv);
-
+void funct_arguments_parser(int argc, char **argv);
+int funct_open_file(int argc, char **argv);
 
 
 //////////////////
 //////////////////
 int main(int argc, char *argv[])
 {
-    const char *options_short = "benstAETuv";
     char buffer[BUFFER_SIZE];  
-    int filename_arg = 0;
+    int filename = 0;
 
     // Use case One - with a file //
     // if arguments exist.
     if (argc > 1)   // if second argument exists (first argument is always an executable name btw) do: 
     {
-        arguments_parser(argc, argv);
-        filename_arg = open(argv[1], O_RDONLY); // tries to open a file
-        scenario_file_doesnt_exist(filename_arg); // if filename is written wrong (or it doesnt exist), prints error
+        // funct_arguments_parser(argc, argv);
+        filename = funct_open_file(argc, argv); // tries to open a file // btw, FIX THIS! IT MUST LOOK FOR FILENAME, not just take argv[1], cuz there could be a flag
+        scenario_file_doesnt_exist(filename); // if filename is written wrong (or it doesnt exist), prints error
         
         ssize_t bytes_length;
-        while ((bytes_length = read(filename_arg, buffer, BUFFER_SIZE))) // while loop, that works until var "bytes_length" is matching number of chars that are schitanni
+        while ((bytes_length = read(filename, buffer, BUFFER_SIZE))) // function "read" returns number of bytes that were read.
         {
-        write(STDOUT_FILENO, buffer, bytes_length); // printf basically
+        write(STDOUT_FILENO, buffer, bytes_length);                  // so it writes the exact number of bytes that func "read" returned.
         }
 
-    close(filename_arg);
+    close(filename);
     }
     // Use case One end. //
-
-
-
 
     // Use case Two - return stdin to stdout (text that user wrote in terminal returns to terminal) //
     // if no arguments are present.
@@ -54,6 +50,26 @@ int main(int argc, char *argv[])
 }
 //////////////////
 //////////////////
+
+int funct_open_file(int argc, char **argv)
+{
+    int fof_buffer;
+
+    for (int i = 0; i < argc ; i++ )
+    {
+        char for_buffer;
+          // okay, so I need to access argv, and read it.
+          // if there are '-' - it means that it's a flag
+          // how? well... duh... will think later
+        getc(for_buffer);
+        if (for_buffer == '-');
+    }
+
+    fof_buffer = open(argv[1], O_RDONLY);
+
+    return fof_buffer;
+}
+
 
 
 
@@ -80,10 +96,10 @@ if (file_descr == -1)   // if file_desc couldn't open anything it will return -1
 }
 
 
-void arguments_parser(int argc,char **argv)
+void funct_arguments_parser(int argc,char **argv)
 {
-
-    static struct cat_flags options_long[] = {
+    const char *options_char = "benstAETuv";
+    static struct option options_string[] = {
         {"show-all",              no_argument, 0, 'A'},
         {"number-nonblank",       no_argument, 0, 'b'},
         {0,                       no_argument, 0, 'e'},
@@ -97,10 +113,9 @@ void arguments_parser(int argc,char **argv)
         {"help",                  no_argument, 0,   0},
         {"version",               no_argument, 0,   0},
         };
-
-
-    int option = 0;
     
+
+
     // small : b e n s t
     //  big  : A E T
     int b_flag = 0;
@@ -119,7 +134,10 @@ void arguments_parser(int argc,char **argv)
     int help_flag = 0;
     int version_flag = 0;
 
-    while ((option = getopt_long(argc, argv, "benstAETuv")) != -1)
+    // buffer with flags, for cycle.
+    int buffer_w_flag;
+
+    while ((buffer_w_flag = getopt_long(argc, argv, options_char, options_string, )) != -1) // 
     {
         switch (option)
         {
