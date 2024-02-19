ab3b9a5714569d674260bf90b014ec8774ead425
Tanjan S21 <saladbox207@protonmail.com>
2024-02-19 17:52:15 +0900

    s21string init

---
+++ src/s21_string.h
@@ -0,0 +1,7 @@
+#define SRC_S21_STRING_H
+#include <stdio.h>
+#include <stdlib.h>
+
+#define s21_NULL ((void*)0) // that's literally a POINTER TO VOID. yep. "NULL" is defined as this
+
+typedef unsigned long s21_size_t;  // and this is "size_t". yeah, literally just a unsigned long int (from 0 to 4,294,967,295)
---
+++ src/string_plus.md
@@ -0,0 +1,4 @@
+soooo, we have another task my darling!
+...
+this time, we'll have to build string h library! zamn
+
