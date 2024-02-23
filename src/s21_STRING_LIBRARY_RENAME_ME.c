e3c80dc298603721121ecf76b9a7880968969b2f
Tanjan S21 <saladbox207@protonmail.com>
2024-02-23 17:25:13 +0900

    add memchr

--- src/s21_STRING_LIBRARY_RENAME_ME.c
+++ src/s21_STRING_LIBRARY_RENAME_ME.c
@@ -1,16 +1,18 @@
 #include "s21_string.h"
 
+// search. it looks for single char in line.
 void *s21_memchr(const void *line, int ch, s21_size_t line_length)
 {
     const unsigned char *pointer = line;
     while(n = n - 1)
     {
-        if (*pointer == (unsigned char)ch)
+        if (*pointer == (unsigned char)ch) // forced cast to unsigned char
         {
-            return (void*)pointer;
+            return (void*)pointer; // forced cast to void (just to be sure)
         }
     }
-    return NULL;
+    // if something went wrong, returns NULL
+    return S21_NULL;
 } 
 
 
