16dad81c2de8bf05c7dfa1a614b36d29a04becf4
Tanjan VM Home <saladbox207@protonmail.com>
2024-08-04 02:32:31 +0900

    ptrs

---
+++ ptrs_ch1x/ch1.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <strings.h>
+
+int main()
+{
+
+    printf("size of a short is %d\n", sizeof(short));
+    printf("size of a int is %d\n", sizeof(int));
+    printf("size of a long is %d\n", sizeof(long));
+
+
+    return EXIT_SUCCESS;
+}
